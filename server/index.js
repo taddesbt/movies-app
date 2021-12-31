77cf49ee0ef7b9bd9496f550bac3ea32f86ccfc6
taddesbt <taddesbt@gmail.com>
2022-01-01 00:14:32 +0300

    backend done tested ok

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const db = require('./db')
+const movieRouter = require('./routes/movie-router')
+
 const app = express()
-const Port = 5000
+const Port = 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/api', movieRouter)
 
 app.listen(Port, () => console.log(`Server running on port ${Port}`))
